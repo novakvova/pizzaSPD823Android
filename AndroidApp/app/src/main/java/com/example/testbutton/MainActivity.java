9babe64cbf9941c7eb1ebfd41b5c47219147efe2
Volodymyr Novak <novakvova@gmail.com>
2020-09-26 15:37:24 +0300

    Show image networking

--- AndroidApp/app/src/main/java/com/example/testbutton/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/testbutton/MainActivity.java
@@ -7,12 +7,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.toolbox.NetworkImageView;
+import com.example.testbutton.network.ImageRequester;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ImageRequester imageRequester;
+    private NetworkImageView editImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageRequester = ImageRequester.getInstance();
+        editImage = findViewById(R.id.chooseImage);
+        imageRequester.setImageFromUrl(editImage,"https://www.regionnews.net.ua/wp-content/uploads/2019/12/zhinky-2.jpg");
     }
     public void onButtonClickTest(View view) {
         Toast toast = Toast.makeText(getApplicationContext(), "Привіт Валера",
--- AndroidApp/app/src/main/java/com/example/testbutton/application/HomeApplication.java
+++ AndroidApp/app/src/main/java/com/example/testbutton/application/HomeApplication.java
@@ -1,4 +1,42 @@
 package com.example.testbutton.application;
 
-public class HomeApplication {
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class HomeApplication extends Application {
+    private static HomeApplication instance;
+    private static Context appContext;
+    private Activity mCurrentActivity = null;
+
+    public static HomeApplication getInstance() {
+        return instance;
+    }
+
+    public static Context getAppContext() {
+        return appContext;
+    }
+
+    public void setAppContext(Context mAppContext) {
+        this.appContext = mAppContext;
+    }
+
+
+    public Activity getCurrentActivity(){
+        return mCurrentActivity;
+    }
+    public void setCurrentActivity(Activity mCurrentActivity){
+        this.mCurrentActivity = mCurrentActivity;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        this.setAppContext(getApplicationContext());
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 }
